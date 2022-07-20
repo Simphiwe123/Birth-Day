9110d415db49c0ef4ee0658188514ff645730b24
Simphiwe <snmkhonza1@gmail.com>
2022-07-20 16:32:24 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import MarqueeText from 'react-native-marquee';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const image = { uri: "https://www.icegif.com/wp-content/uploads/happy-birthday-icegif-6.gif" };
+
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>To Simphiwe Mkhonza</Text>
+
+      <MarqueeText
+          style={{  color: "white",
+          fontSize: 30,
+          lineHeight: 64,
+          fontWeight: "bold",
+          textAlign: "center",
+          backgroundColor: "#000000c0" }}
+          speed={0.5}
+          marqueeOnStart={true}
+          loop={true}
+          delay={1000} 
+        >
+         I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy!
+        </MarqueeText>
+        <Text style={styles.text}>Hope your day goes great!</Text>
+    </ImageBackground>
+    
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  },
+  txt: {
+    color: "white",
+    fontSize: 30,
+    lineHeight: 64,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+
 });
+
+export default App;
